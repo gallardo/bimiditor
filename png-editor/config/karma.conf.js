720dfd8e6b74e37bdd92716f3d9eb029b24da3d8
Alberto <rusgall@rus.uni-stuttgart.de>
2013-05-15 10:53:07 +0200

    Restructure js libs to easy testing config

--- png-editor/config/karma.conf.js
+++ png-editor/config/karma.conf.js
@@ -3,10 +3,11 @@ basePath = '../';
 files = [
   JASMINE,
   JASMINE_ADAPTER,
-  'app/lib/angular/angular.js',
-  'app/lib/angular/angular-*.js',
+  'public_html/lib/angular/angular.js',
+  'public_html/lib/angular/angular-*.js',
+  'public_html/lib/jquery-*.js',
   'test/lib/angular/angular-mocks.js',
-  'app/js/**/*.js',
+  'public_html/js/**/*.js',
   'test/unit/**/*.js'
 ];
 

63ebd75738651c4d2f19c11e49f057de8ca25740
Alberto <rusgall@rus.uni-stuttgart.de>
2013-05-17 19:28:56 +0200

    Separate angular code according to responsibilities
    Correct and separate application's angular code into controllers,
    directives, filters and services.

--- png-editor/public_html/js/controllers.js
+++ png-editor/public_html/js/controllers.js
@@ -1,24 +1,10 @@
 'use strict';
 
 /* Controllers */
-
-angular.module('myApp.controllers', []).
-  controller('MyCtrl1', [function() {
-
-  }])
-  .controller('MyCtrl2', [function() {
-
-  }]);
-
-// angular.module('binPngEditorApp.controllers', []);
-
-function BinPngEditorCtrl($scope, $window) {
-    BinPngEditorCtrl.prototype.$scope = $scope;
-}
-
-BinPngEditorCtrl.prototype.setFile = function(element) {
-    var $scope = this.$scope;
-    $scope.$apply(function() {
-        $scope.pngFile = element.files[0];
-    });
-};
+angular.module('binPngEditorApp.controllers', [])
+        .controller('BinPngEditorCtrl', ['$scope', function($scope) {
+        this.scope = $scope;
+        this.setFile = function(element) {
+            scope.pngFile = element.files[0];
+        };
+    }]);

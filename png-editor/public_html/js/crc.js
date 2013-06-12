6004d5aff1112df8c02ce1d1abac396c12bf8e1b
Travis <ag-travis-ci@gmail.com>
2013-06-12 11:34:51 +0200

    Fix CRC-32 calculation and create its tests

--- png-editor/public_html/js/crc.js
+++ png-editor/public_html/js/crc.js
@@ -1,55 +1,80 @@
 /**
+ * To synch the four-byte CRC (Cyclic Redundancy Code) calculated on the
+ * preceding bytes in the chunk, including the chunk type field and chunk
+ * data fields, but not including the length field.
+ *
  * @see http://www.w3.org/TR/PNG/#D-CRCAppendix
  */
 
-/*
- * {Array of 256 unsigned long} Table of CRCs of all 8-bit messages.
- */
-var crcTable;
+function Crc() {
+	/**
+	 * {Array of 256 unsigned long} Table of CRCs of all 8-bit messages.
+	 */
+	var _crcTable;
 
-/* Make the table for a fast CRC. */
-function makeCrcTable()
-{
-	crcTable = [];
-	for (var n = 0; n < 256; n++) {
-		var c = n;
-		for (var k = 0; k < 8; k++) {
-			if (c & 1)
-				c = 0xedb88320 ^ (c >> 1);
-			else
-				c = c >> 1;
+	/** CRC polynomial employed */
+	var _POLYN = 0xedb88320;
+
+	/* Make the table for a fast CRC. */
+	function _makeCrcTable()
+	{
+		_crcTable = [];
+		for (var n = 0; n < 256; n++) {
+			var c = n;
+			for (var k = 0; k < 8; k++) {
+				c = ((c & 1) === 1)
+					? _POLYN ^ (c >>> 1)
+					: c >>> 1;
+			}
+			_crcTable[n] = c;
 		}
-		crcTable[n] = c;
+	};
+
+	function _printTable() {
+			var hexValues = "";
+			for (var i = 0; i < _crcTable.length; ++i) {
+				hexValues += "0x" + (_crcTable[i] >>> 0).toString(16) + ", ";
+			}
+			console.log("table: " + hexValues);		
 	}
-}
 
-/*
- * Update a running CRC with the bytes buf[0..len-1]--the CRC
- * should be initialized to all 1's, and the transmitted value
- * is the 1's complement of the final running CRC (see the
- * crc() routine below).
- *
- * @param {unsigned long} crc
- * @param {unsigned char *} buf
- * @param {int} len
- * @return {unsigned long}
- */
-function updateCrc(crc, buf, len) {
-	var c = crc;
+	/*
+	 * Update a running CRC with the bytes buf[0..len-1]--the CRC
+	 * should be initialized to all 1's, and the transmitted value
+	 * is the 1's complement of the final running CRC (see the
+	 * crc() routine below).
+	 *
+	 * @param {unsigned long} crc
+	 * @param {ArrayBuffer} buf
+	 * @param {unsigned long} offset
+	 * @param {unsigned long} len
+	 * @return {unsigned long}
+	 */
+	function _updateCrc(crc, buf, offset, len) {
+		var c = crc;
+		var bufView = new DataView(buf, offset, len);
 
-	if (!crcTable)
-		makeCrcTable();
-	for (var n = 0; n < len; n++) {
-		c = crcTable[(c ^ buf[n]) & 0xff] ^ (c >> 8);
-	}
-	return c;
-}
+		if (!_crcTable) {
+			_makeCrcTable();
+			console.log("CRC-32 table created");
+			// _printTable();
+		}
+		for (var n = offset; n < len; n++) {
+			c = _crcTable[(c ^ bufView.getUint8(n)) & 0xff] ^ (c >>> 8);
+		}
+		return c;
+	};
 
-/*
- * @param {unsigned char *} buf
- * @param {int} len
- * @return {unsigned long} CRC of the bytes buf[0..len-1].
- */
-function crc(buf, len) {
-	return updateCrc(0xffffffff, buf, len) ^ 0xffffffff;
+	var _that = {
+		/*
+		 * @param {ArrayBuffer} buf
+		 * @param {unsigned long} offset
+		 * @param {unsigned long} length
+		 * @return {unsigned long} CRC of the bytes buf[0..len-1].
+		 */
+		calculate: function(buf, offset, len) {
+			return _updateCrc(0xffffffff, buf, offset, len) ^ 0xffffffff;
+		}
+	};
+	return _that;
 }
---
+++ png-editor/test/unit/crcSpec.js
@@ -0,0 +1,53 @@
+"use strict";
+
+/**
+ * @param {Number} n
+ * @return {String} unsigned hexadecimal string representation
+ */
+function toHex(n) {
+    return "0x" + (n >>> 0).toString(16);
+}
+
+/**
+ * Array of bytes (unsigned char[]) to ArrayBuffer
+ *
+ * @param {unsigned char[]} n array of bytes <tt>(unsigned char[])</tt>
+ * @return {ArrayBuffer}
+ */
+function ucatoab(n) {
+    var buf = new ArrayBuffer(n.length);
+    var view = new DataView(buf);
+    for (var i = 0; i < n.length; ++i) {
+        view.setUint8(i, n[i]);
+    }
+    return buf;
+}
+
+describe("Crc suite", function() {
+    it("should calculate the CRC of an 'IEND' chunk", function() {
+        // Example from a real png chunk:
+        // 49 45 4E 44 (type/name)
+        // AE 42 60 82 (CRC)
+        var bytes = ucatoab([0x49, 0x45, 0x4E, 0x44]);
+        var expectedCRC = 0xae426082;
+
+        var calculatedCRC = Crc().calculate(bytes, 0, bytes.byteLength);
+        expect(toHex(calculatedCRC)).toBe(toHex(expectedCRC));
+    });
+    it("should calculate the CRC of the ASCII representation of '1'", function() {
+        // Example and result calculated with http://www.lammertbies.nl/comm/info/crc-calculation.html
+        var bytes = ucatoab([0x31]);
+        var expectedCRC = 0x83DCEFB7;
+
+        var calculatedCRC = Crc().calculate(bytes, 0, bytes.byteLength);
+        expect(toHex(calculatedCRC)).toBe(toHex(expectedCRC));
+    });
+    it("should calculate the CRC of the ASCII representation of '123456789'", function() {
+        // Example and result calculated with http://www.lammertbies.nl/comm/info/crc-calculation.html
+        var bytes = ucatoab([0x31,0x32,0x33,0x34,0x35,0x36,0x37,0x38,0x39]);
+        var expectedCRC = 0xCBF43926;
+
+        var calculatedCRC = Crc().calculate(bytes, 0, bytes.byteLength);
+        expect(toHex(calculatedCRC)).toBe(toHex(expectedCRC));
+    });
+});
